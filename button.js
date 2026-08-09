12defa16c75db58c4d53464c08fcc221e8132e2a
piccb88vip-art <pic_cb88vip@clickgroup.id>
2026-08-10 06:41:21 +0700

    Update button.js

--- button.js
+++ button.js
@@ -28,7 +28,7 @@
       z-index: 999999;
       right: 10px;
       top: 82%;
-      width: 155px;
+      width: 135px;
       height: auto;
       display: block;
       padding: 0;
@@ -133,7 +133,7 @@
         top: auto;
         right: 7px;
         bottom: calc(95px + env(safe-area-inset-bottom, 0px));
-        width: 105px;
+        width: 90px;
         transform: none;
       }
     }
